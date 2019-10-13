93070e10a8a8be668c49e30a0a85f20f9bd71845
cmlee <bal.dongdongdong@gmail.com>
2019-10-14 02:49:16 +0900

    update bug

--- build/utils/createJWT.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-var jsonwebtoken_1 = __importDefault(require("jsonwebtoken"));
-var createJWT = function (id) { return jsonwebtoken_1.default.sign({ id: id }, process.env.JWT_SECRET || ''); };
-exports.default = createJWT;
-//# sourceMappingURL=createJWT.js.map
